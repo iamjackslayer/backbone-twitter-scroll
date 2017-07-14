2522158cac3e57cea5b27b819ab5e88b7449677a
Jack Wong <jackwong3101@yahoo.com>
2017-07-14 20:42:56 +0800

    .

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,4 +8,16 @@ class StaticPagesController < ApplicationController
 		end
 		@tweets = client.user_timeline('rubyinside',count: 20) 
 	end
+
+	def search
+		client = Twitter::REST::Client.new do |config|
+		  config.consumer_key = '2ata698MwiwPUAMxXg2Na99c1'
+		  config.consumer_secret = 'MC3UuF6OA8WkJjORxfqSEQVlEVs5TGlUwzHkDRI4UX5yotQz5n'
+		  config.access_token = '2925112608-EtrVoSRCKK2pHTxCTu2S8sKZXbiJ1O45p5PQVfR'
+		  config.access_token_secret = 'OPcK8sjXWq6lERiL6vsuTpHoDwSQ1qOBpOqW9vrC8ZmB2'
+		end
+		@tweets = client.user_timeline('rubyinside',count: 20)
+
+		render json: @tweets
+	end
 end
