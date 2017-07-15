4d46215a3fd325f6b2f5048035470c8edfe14fc6
Jack Wong <jackwong3101@yahoo.com>
2017-07-15 10:18:22 +0800

    .

--- app/assets/javascripts/backbone/index.js
+++ app/assets/javascripts/backbone/index.js
@@ -36,8 +36,8 @@ var tweets = new Tweet();
 var Form = Backbone.View.extend({
 	el:'#inputForm',
 	events: {
-		'click button':'request',
-		'click button':'clearViewList'
+		'submit':'request',
+		'submit':'clearViewList'
 	},
 	request: function(){
 		var queryInput = $(this.el).find('#search').val();
