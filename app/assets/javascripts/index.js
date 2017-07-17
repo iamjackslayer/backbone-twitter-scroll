ce51bc5901efeecebdcd00d7982c69e23d713db0
Jack Wong <jackwong3101@yahoo.com>
2017-07-17 14:28:46 +0800

    .

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -131,7 +131,7 @@ var TwitterWidget = Backbone.View.extend({
 	},
 	loadResults: function(){
 		console.log("data appended to the template");
-		$(this.el).append(this.template({tweets: this.collection.models}));
+		loadingSpinner.before(this.template({tweets: this.collection.models}));
 	},
 	checkScroll: function(){
 		console.log("scrolling.....................................................");
